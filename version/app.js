2d66c66190d8e627391cb094bd2c318a38730c66
Wronnnn <skowronmaciej@protonmail.com>
2024-12-27 13:37:46 +0100

    v6

--- version/app.js
+++ version/app.js
@@ -4,7 +4,7 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-    response = 'This is version 5 of the app.' + '\n';
+    response = 'This is version 6 of the app.' + '\n';
 
     res.send(response);
 
